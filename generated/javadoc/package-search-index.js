63d34a0ed52dd256ce4dcb39c9ae16543910be92
Paul Harrison <paul.harrison@manchester.ac.uk>
2024-08-20 14:34:02 +0100

    Deployed 113d6ab with MkDocs version: 1.6.0

---
+++ generated/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.ivoa.dm.caom2"},{"l":"org.ivoa.dm.caom2.caom2"},{"l":"org.ivoa.dm.caom2.caom2.types"},{"l":"org.ivoa.dm.caom2.caom2.wcs"}];updateSearchResults();
